b150a325f643ff5b2ecf7a5f5e6d9b3cf9fb116e
Gian <giansantos.dev@gmail.com>
2023-10-12 17:36:22 -0300

    add back button to second screen and update colors

--- app/src/main/java/com/example/electriccarapp/presentation/CalcularAutonomiaActivity.kt
+++ app/src/main/java/com/example/electriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -3,9 +3,11 @@ package com.example.electriccarapp.presentation
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.electriccarapp.R
+import java.lang.NumberFormatException
 import java.text.DecimalFormat
 import java.util.Locale
 
@@ -14,6 +16,7 @@ class CalcularAutonomiaActivity : AppCompatActivity() {
     lateinit var kmPercorridos: EditText
     lateinit var resultado: TextView
     lateinit var btnEnviar: Button
+    lateinit var btnVoltar: ImageView
     val numberFormatter = DecimalFormat.getCurrencyInstance(Locale("pt", "BR"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,15 +31,28 @@ class CalcularAutonomiaActivity : AppCompatActivity() {
         kmPercorridos = findViewById(R.id.et_km_percorrido)
         btnEnviar = findViewById(R.id.btn_enviar)
         resultado = findViewById(R.id.tv_resultado)
+        btnVoltar = findViewById(R.id.btn_back)
     }
 
     private fun setupListeners() {
+        btnVoltar.setOnClickListener{
+            finish()
+        }
         btnEnviar.setOnClickListener {
-            val precoValue = preco.text.toString().toFloat()
-            val kmPercorridoValue = kmPercorridos.text.toString().toFloat()
+            try {
+                val precoValue = preco.text.toString().toFloat()
+                val kmPercorridoValue = kmPercorridos.text.toString().toFloat()
 
-            val result = precoValue / kmPercorridoValue
-            resultado.text = numberFormatter.format(result)
+                val result = precoValue / kmPercorridoValue
+
+                resultado.text = numberFormatter.format(result)
+
+            } catch (e: NumberFormatException) {
+                // TODO: Mostrar um erro que os valores estão vazios
+            } catch (e: ArithmeticException) {
+                // TODO: Mostrar um erro de divisão por 0
+            }
+
         }
     }
 
