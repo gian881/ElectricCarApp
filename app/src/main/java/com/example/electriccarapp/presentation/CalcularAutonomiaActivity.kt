ffe80acd158d94bb41ca0d5ceeebf55a87a381d9
Gian <giansantos.dev@gmail.com>
2023-10-12 15:25:09 -0300

    late initial commit

---
+++ app/src/main/java/com/example/electriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -0,0 +1,41 @@
+package com.example.electriccarapp.presentation
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.electriccarapp.R
+
+class CalcularAutonomiaActivity : AppCompatActivity() {
+    lateinit var preco: EditText
+    lateinit var kmPercorridos: EditText
+    lateinit var resultado: TextView
+    lateinit var btnEnviar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calcular_autonomia)
+        setupView()
+        setupListeners()
+    }
+
+    private fun setupView() {
+        preco = findViewById(R.id.et_preco_kwh)
+        kmPercorridos = findViewById(R.id.et_km_percorrido)
+        btnEnviar = findViewById(R.id.btn_enviar)
+        resultado = findViewById(R.id.tv_resultado)
+    }
+
+    private fun setupListeners() {
+        btnEnviar.setOnClickListener {
+            val precoValue = preco.text.toString().toFloat()
+            val kmPercorridoValue = kmPercorridos.text.toString().toFloat()
+
+            val result = precoValue / kmPercorridoValue
+
+            resultado.text = result.toString()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.electriccarapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.electriccarapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
