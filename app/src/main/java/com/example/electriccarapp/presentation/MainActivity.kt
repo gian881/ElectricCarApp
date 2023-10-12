88e1538b6722c55ee6208a8de700b81f19fd9d4c
Gian <giansantos.dev@gmail.com>
2023-10-12 18:43:44 -0300

    utilizing a recyclerview and a custom adapter to show cars dinamically on screen

--- app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
@@ -3,21 +3,38 @@ package com.example.electriccarapp.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.electriccarapp.R
+import com.example.electriccarapp.presentation.adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnCalcularAutonomia: FloatingActionButton
+    lateinit var listaCarros: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupView()
         setupListeners()
+        setupList()
     }
 
     private fun setupView() {
         btnCalcularAutonomia = findViewById(R.id.fab_calculate)
+        listaCarros = findViewById(R.id.rv_car_list)
+    }
+
+    private fun setupList() {
+        val data = arrayOf(
+            "R$ 1.322.000,00",
+            "R$ 952.280,00",
+            "R$ 2.222.222,00",
+            "R$ 3.000.312,00"
+        )
+
+        val adapter = CarAdapter(data)
+        listaCarros.adapter = adapter
     }
 
     private fun setupListeners() {
