04044695517a803bc40701a9782ee2e5ec6eb661
Gian <giansantos.dev@gmail.com>
2023-10-12 16:42:34 -0300

    added main screen

--- app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.electriccarapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import com.example.electriccarapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnCalcularAutonomia: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+        setupListeners()
+    }
+
+    private fun setupView() {
+        btnCalcularAutonomia = findViewById(R.id.fab_calculate)
+    }
+
+    private fun setupListeners() {
+        btnCalcularAutonomia.setOnClickListener {
+            startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
+        }
     }
 }
